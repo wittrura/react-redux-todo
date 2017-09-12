4dbf04b7ec20518260f70f0b70e12f9b22078577
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-09-12 18:20:14 -0500

    core: creates reducers file for managing state changes on todos and filters

--- src/App.js
+++ src/App.js
@@ -2,40 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      txt: 'this is the state text',
-    };
-  }
-
-  update(e) {
-    this.setState({txt: e.target.value});
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>{this.state.txt}</h1>
-        <Widget update={this.update.bind(this)}/>
-      </div>
-    );
-  }
+  render() {}
 }
 
-const Widget = (props) => {
-  return (
-    <input type="text" onChange={props.update}/>
-  );
-};
-
-App.propTypes = {
-  txt: PropTypes.string,
-  cat: PropTypes.number.isRequired
-};
-
-App.defaultProps = {
-  txt: 'this is the default txt'
-};
 
 export default App;
