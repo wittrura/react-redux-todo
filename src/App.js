76dd81651374c316a59d8932e780deb9075ced50
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-09-12 17:10:48 -0500

    refactor: delete unnecessary files, create actions file for redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state text',
+    };
+  }
+
+  update(e) {
+    this.setState({txt: e.target.value});
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)}/>
       </div>
     );
   }
 }
+
+const Widget = (props) => {
+  return (
+    <input type="text" onChange={props.update}/>
+  );
+};
+
+App.propTypes = {
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+  txt: 'this is the default txt'
+};
 
 export default App;
